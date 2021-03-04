eba0b076ab96dbf72246ec0dd3e0db84a45b872c
Diogo Novais <diogo.vieiranovais@gmail.com>
2021-03-03 21:07:45 -0300

    Projeto Criado

---
+++ src/course/aplication.java
@@ -0,0 +1,14 @@
+package course;
+
+public class aplication {
+
+	public aplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
